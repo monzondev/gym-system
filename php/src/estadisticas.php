b2edc9c0bf88cccbd1a11bcc5040db58f9f27a43
Juan Pleitez <juanpc13lolol@gmail.com>
2019-10-06 23:08:30 -0600

    Creación páginas html con mensaje “e' desarrolloâ'

--- php/src/estadisticas.php
+++ php/src/estadisticas.php
@@ -2,13 +2,13 @@
 <head>
     <meta charset='utf-8'>
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-    <title>Gym System</title>
+    <title>Estadisticas</title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
     <link rel='stylesheet' type='text/css' media='screen' href='css/reset.css'>
     <link rel='stylesheet' type='text/css' media='screen' href='css/simple-grid.css'>
 </head>
 <body>
-    <div class="row">
+<div class="row">
         <div class="col-1"></div>
         <div class="col-10" style="height:100vh;">
             <h1>En Desarrollo</h1>
--- php/src/miembros.php
+++ php/src/miembros.php
@@ -2,7 +2,7 @@
 <head>
     <meta charset='utf-8'>
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-    <title>Gym System</title>
+    <title>Miembros</title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
     <link rel='stylesheet' type='text/css' media='screen' href='css/reset.css'>
     <link rel='stylesheet' type='text/css' media='screen' href='css/simple-grid.css'>
@@ -14,7 +14,7 @@
             <h1>En Desarrollo</h1>
         </div>
         <div class="col-1"></div>
-    </div>
+    </div>    
     
 </body>
 </html>

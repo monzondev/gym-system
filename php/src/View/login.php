81934801f9a697eeb0ce08343085ae50d5ff8670
Alexander Monzon <alexandermm2011@gmail.com>
2019-10-07 23:40:08 -0600

    codificacion interfaz de inicio de sesion

--- php/src/View/login.php
+++ php/src/View/login.php
@@ -1,20 +1,41 @@
 <html>
+
 <head>
-    <meta charset='utf-8'>
-    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-    <title>Inicio de Session</title>
-    <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <link rel='stylesheet' type='text/css' media='screen' href='Style/css/reset.css'>
-    <link rel='stylesheet' type='text/css' media='screen' href='Style/css/simple-grid.css'>
+	<meta charset='utf-8'>
+	<meta http-equiv='X-UA-Compatible' content='IE=edge'>
+	<title>Inicio de Session</title>
+	<meta name='viewport' content='width=device-width, initial-scale=1'>
+	<link rel='stylesheet' type='text/css' media='screen' href='Style/css/reset.css'>
+	<link rel='stylesheet' type='text/css' media='screen' href='Style/css/simple-grid.css'>
 </head>
+
 <body>
-    <div class="row">
-        <div class="col-1"></div>
-        <div class="col-10" style="height:100vh;">
-            <h1>En Desarrollo</h1>
-        </div>
-        <div class="col-1"></div>
-    </div>
-    
+	<br><br><br><br><br><br>
+	<center>
+		<form name="form-login" id="loginform" method="post" action="" style="width:30%;">
+				
+			<fieldset>
+			<h2>Inicio de sesion</h2>
+				<br><br>
+				<div class="form-group">
+					<input class="form-control" placeholder="Usuario" name="usuario" type="text" autofocus="" style="width: 70%;">
+				</div>
+				<br>
+				<div class="form-group">
+					<input class="form-control" placeholder="Clave" name="clave" type="password" value="" style="width: 70%;">
+				</div>
+				<br>
+				<div class="checkbox">
+					<label>
+						<input name="recordar" type="checkbox" value="Recordarme">Recordarme
+					</label>
+				</div>
+				 <br><br>
+				<button type="submit" class="btn btn-twitter btn-block btn-flat" style="width: 30%">Iniciar</button>
+				<br>
+			</fieldset>
+		</form>
+	</center>
 </body>
+
 </html>

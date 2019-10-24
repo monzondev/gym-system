52eb5ec543bab4ed7e2c167ad96b152a116351b2
Alexander Monzon <alexandermm2011@gmail.com>
2019-10-24 09:10:33 -0600

    agregando id a las opciones del menu

--- php/src/view/navbar.php
+++ php/src/view/navbar.php
@@ -13,7 +13,7 @@ $tipo = $tipoEmpleado->getTipoEmpleado($_SESSION['tipoEmpleado']);
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
-                <a class="nav-link" href="index.php" style="padding-right: 50px; ">DashBoard</a>
+                <a class="nav-link" href="index.php" id="home" style="padding-right: 50px; ">DashBoard</a>
             </li>
             <li class="nav-item dropdown active">
                 <?php if ($_SESSION['tipoEmpleado'] == 1) { ?>
@@ -25,32 +25,32 @@ $tipo = $tipoEmpleado->getTipoEmpleado($_SESSION['tipoEmpleado']);
                     Miembros
                 </a>
                 <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="registrar-miembro.php">Registrar</a>
-                    <a class="dropdown-item" href="miembros.php">Ver todos</a>
+                    <a class="dropdown-item" id="newMiembro" href="registrar-miembro.php">Registrar</a>
+                    <a class="dropdown-item" id="Allmiembros" href="miembros.php">Ver todos</a>
                 </div>
             </li>
             <?php if ($_SESSION['tipoEmpleado'] == 1) { ?>
                 <li class="nav-item dropdown active">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" style="padding-right: 50px; " role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <a class="nav-link dropdown-toggle" href="" id="navbarDropdown" style="padding-right: 50px; " role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         Empleados
                     </a>
                     <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="registrar-empleado.php">Registrar</a>
-                        <a class="dropdown-item" href="empleados.php">Ver todos</a>
+                        <a class="dropdown-item"  id="newEmpleado" href="registrar-empleado.php">Registrar</a>
+                        <a class="dropdown-item" id="AllEmpleados" href="empleados.php">Ver todos</a>
                     </div>
                 </li>
                 <li class="nav-item active">
-                    <a class="nav-link" href="estadisticas.php" style="padding-right: 25vw" ;>Estadisticas</a>
+                    <a class="nav-link" href="estadisticas.php"  id="estadisticas" style="padding-right: 25vw" ;>Estadisticas</a>
                 </li>
             <?php } ?>
 
             <li class="nav-item dropdown active" style="float: left;">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" style="float: left;" "role=" button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <a class="nav-link dropdown-toggle" href="" id="navbarDropdown" style="float: left;"  id="cuenta"" role=" button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     Cuenta
                 </a>
                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                     <a class="dropdown-item">Nombre</a>
-                    <a class="dropdown-item" style="cursor: pointer;" onclick="location.href='../controller/loginController.php?close=1';">Cerrar Sesi&oacute;n</a>
+                    <a class="dropdown-item" style="cursor: pointer;" id="logout" onclick="location.href='../controller/loginController.php?close=1';">Cerrar Sesi&oacute;n</a>
                 </div>
             </li>
         </ul>

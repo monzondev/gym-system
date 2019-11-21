1b920e5022a6a6247fc9700d27d47fe8c73e6c15
Juan Pleitez <juanpc13lolol@gmail.com>
2019-11-21 15:26:48 -0600

    Agregando validacion al selecionar fecha

--- php/src/controller/estadisticaController.php
+++ php/src/controller/estadisticaController.php
@@ -8,28 +8,39 @@ $empleadoFacade = new empleado();
 
 if (isset($_GET['ingresosPorDia']) && $_GET['ingresosPorDia']) {
     $json = file_get_contents('php://input');
-    $fecha = (json_decode($json))->fecha;
+    $fecha = (json_decode($json))->fecha;    
     $id_empleado = (json_decode($json))->id_empleado;
     $empleado = (object) $empleadoFacade->getUserbyId($id_empleado);
     if(isset($empleado) && $empleado->id_tipo_empleado == 1){
-        $end = new DateTime($fecha);
-        $begin = new DateTime($end->format("Y-m-d"));
-        $begin->sub(new DateInterval('P7D'));
-
-        $interval = DateInterval::createFromDateString('1 day');
-        $period = new DatePeriod($begin, $interval, $end);
-        $ingresos = new stdClass();
-        $ingresos->labels = array();
-        $ingresos->data = array();
-        foreach ($period as $dt) {            
-            $dias = array("Lunes","Martes","Miercoles","Jueves","Viernes","Sábado", "Domingo");
-            $dayName = $dias[$dt->format('N')-1];
-            $dayName = $dayName . " " . $dt->format("d");
-            $income = $miembroFacade->obternerVentas($dt->format("Y-m-d"));
-            array_push($ingresos->data, $income);
-            array_push($ingresos->labels, $dayName);
+        if($fecha == ''){
+            $end = new DateTime('');
+            $date = $end;
+        }else{
+            $end = DateTime::createFromFormat('Y-m-d', $fecha);
         }        
-        echo json_encode($ingresos);        
+        if($end <= new DateTime()){
+            $begin = new DateTime($end->format("Y-m-d"));
+            $begin->sub(new DateInterval('P7D'));
+
+            $interval = DateInterval::createFromDateString('1 day');
+            $period = new DatePeriod($begin, $interval, $end);
+            $ingresos = new stdClass();
+            $ingresos->labels = array();
+            $ingresos->data = array();
+            foreach ($period as $dt) {
+                $dias = array("Lunes","Martes","Miercoles","Jueves","Viernes","Sábado", "Domingo");
+                $mes = ['Ene','Feb','Mar','Abr','May','Jun','Jul','Ago','Sep','Oct','Nov','Dic'];
+                $dayName = $dias[$dt->format('N')-1];
+                $dayName = $dayName . " " . $dt->format("d") . " " .$mes[$dt->format("m")-1];
+                $income = $miembroFacade->obternerVentas($dt->format("Y-m-d"));
+                array_push($ingresos->data, $income);
+                array_push($ingresos->labels, $dayName);
+            }        
+            echo json_encode($ingresos);
+        }else{
+            $response = array('message' => 'No se permiten fechas futuras', 'code' => 2);
+            echo json_encode($response);
+        }        
     }else{
         $response = array('message' => 'No tiene permisos de administrador', 'code' => 2);
         echo json_encode($response);

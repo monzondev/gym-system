b012e113263947d375224cce136beaf3c4dcc509
Juan Pleitez <juanpc13lolol@gmail.com>
2019-11-21 04:02:47 -0600

    Agregando las estadisticas

---
+++ php/src/controller/estadisticaController.php
@@ -0,0 +1,61 @@
+<?php
+include_once '../boundary/pago.php';
+include_once '../boundary/miembro.php';
+include_once '../boundary/empleado.php';
+$pagoFacade = new pago();
+$miembroFacade = new miembro();
+$empleadoFacade = new empleado();
+
+if (isset($_GET['ingresosPorDia']) && $_GET['ingresosPorDia']) {
+    $json = file_get_contents('php://input');
+    $fecha = (json_decode($json))->fecha;
+    $id_empleado = (json_decode($json))->id_empleado;
+    $empleado = (object) $empleadoFacade->getUserbyId($id_empleado);
+    if(isset($empleado) && $empleado->id_tipo_empleado == 1){
+        $end = new DateTime($fecha);
+        $begin = new DateTime($end->format("Y-m-d"));
+        $begin->sub(new DateInterval('P7D'));
+
+        $interval = DateInterval::createFromDateString('1 day');
+        $period = new DatePeriod($begin, $interval, $end);
+        $ingresos = new stdClass();
+        $ingresos->labels = array();
+        $ingresos->data = array();
+        foreach ($period as $dt) {            
+            $dias = array("Domingo","Lunes","Martes","Miercoles","Jueves","Viernes","Sábado");
+            $dayName = $dias[$dt->format('N')-1];
+            $dayName = $dayName . " " . $dt->format("d");
+            $income = $miembroFacade->obternerVentas($dt->format("Y-m-d"));
+            array_push($ingresos->data, $income);
+            array_push($ingresos->labels, $dayName);
+        }        
+        echo json_encode($ingresos);        
+    }else{
+        $response = array('message' => 'No tiene permisos de administrador', 'code' => 2);
+        echo json_encode($response);
+    }
+    exit();
+}
+
+if (isset($_GET['miembrosPorEstado']) && $_GET['miembrosPorEstado']) {
+    $json = file_get_contents('php://input');    
+    $id_empleado = (json_decode($json))->id_empleado;
+    $empleado = (object) $empleadoFacade->getUserbyId($id_empleado);
+    if(isset($empleado) && $empleado->id_tipo_empleado == 1){        
+        $miembros = new stdClass();
+        $miembros->labels = array("Activos", "Pendientes", "Inactivos");
+        $activos = $miembroFacade->countByEstado(1);
+        $pendientes = $miembroFacade->countByEstado(2);
+        $inactivos = $miembroFacade->countByEstado(3);
+        $miembros->data = array($activos, $pendientes, $inactivos);              
+        echo json_encode($miembros);        
+    }else{
+        $response = array('message' => 'No tiene permisos de administrador', 'code' => 2);
+        echo json_encode($response);
+    }
+    exit();
+}
+
+header('Location: /view/index.php');
+exit();
+?>
--- php/src/view/estadisticas.php
+++ php/src/view/estadisticas.php
@@ -13,23 +13,38 @@ if ($_SESSION['tipoEmpleado']!=1) {
     <meta charset='utf-8'>
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
     <title>Estadisticas</title>
-    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>    
     <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/toastr.css">
     <link rel="icon" type="image/png" href="img/favicon.png">
 </head>
 <body>
     <?php include_once("navbar.php"); ?>
     <div class="row">
         <div class="col-md-1"></div>
-        <div class="col-md-10">
-            <h1>En Desarrrolo</h1>
+        <div class="col-md-10 text-center">            
+            <h1>Ingresos de por d&#237;a</h1>
+            <input type="date" class="form-control" style="width: 50%; display: inline-block;" id="fecha">
+            <input class="btn btn-info" id="btn_fecha" type="button" value="Buscar">
+            <br><br>
+            <canvas id="grafica-ingresos" style=""></canvas>
+            <br><hr><br>
+            <h1>Miembros Por Estado</h1>
+            <br>
+            <canvas id="miembros_por_estado" style=""></canvas>
+            <br><hr><br>
+            <h1>Total de miembros registrados</h1>
+            <h1>0</h1>
+            <br><hr><br>
         </div>
         <div class="col-md-1"></div>
     </div>
     <script src="js/jQuery-3-4.1.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
+    <script src="js/mdb.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/toastr.js"></script>
     <script>
-        /*
         $('#miembrosOptions').hover(function() {
             $('#navbarDropdownMiembros').trigger('click')
         })
@@ -40,7 +55,138 @@ if ($_SESSION['tipoEmpleado']!=1) {
         $('#empleadosOptions').hover(function() {
             $('#empleados').trigger('click')
         })
-        */
+    </script>
+    <script>
+        <?php
+            $date = new DateTime("now", new DateTimeZone('America/El_Salvador'));
+        ?>
+        var date = "<?php echo $date->format('Y-m-d');?>";
+
+        //Poner fecha de hoy al filto de la fecha
+        $('#fecha').val(date);        
+        $('#btn_fecha').click(function() {
+            var f = $('#fecha').val();
+            updateIngresosPorDia(f);
+        });
+
+        function updateIngresosPorDia(fecha){
+            var id_empleado = <?php echo $_SESSION['idEmpleado']; ?>;
+            $.ajax({
+                type: "POST",
+                async: false,
+                url: "../controller/estadisticaController.php?ingresosPorDia=true",
+                data: JSON.stringify({
+                    "id_empleado": id_empleado, "fecha": fecha
+                }),
+                success: function(data) {
+                    var response = jQuery.parseJSON(data);
+                    if (typeof response.code !== 'undefined') {
+                        toastr.error(response.message);
+                    } else {
+                        if(typeof myChart !== 'undefined'){
+                            myChart.destroy();
+                        }
+                        llenarGraficaBar("grafica-ingresos", response);
+                    }
+                }
+            });
+        }
+
+        function updateMiembroPorEstado(){
+            var id_empleado = <?php echo $_SESSION['idEmpleado']; ?>;
+            $.ajax({
+                type: "POST",
+                async: false,
+                url: "../controller/estadisticaController.php?miembrosPorEstado=true",
+                data: JSON.stringify({
+                    "id_empleado": id_empleado
+                }),
+                success: function(data) {
+                    var response = jQuery.parseJSON(data);
+                    if (typeof response.code !== 'undefined') {
+                        toastr.error(response.message);
+                    } else {
+                        llenarGraficaPie("miembros_por_estado", response);
+                    }
+                }
+            });
+        }
+
+        
+
+        $(document).ready(function() {
+            var f = $('#fecha').val();
+            updateIngresosPorDia(f);            
+            updateMiembroPorEstado();
+        });
+
+
+    </script>
+    <script>
+        var myChart;
+        //llenarGrafica(document.getElementById("myChart").getContext('2d'), d)
+        function llenarGraficaBar(id, d){            
+            var ctx = document.getElementById(id).getContext('2d');
+            //var ctx = document.getElementById("myChart").getContext('2d');
+            myChart = new Chart(ctx, {
+                type: 'bar',
+                data: {
+                    labels: d.labels,
+                    datasets: [{
+                        label: 'Ingresos $',
+                        data: d.data,
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(255, 206, 86, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(153, 102, 255, 0.2)',
+                            'rgba(45, 102, 255, 0.2)',
+                            'rgba(255, 159, 64, 0.2)'
+                        ],
+                        borderColor: [
+                            'rgba(255,99,132,1)',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 1)',
+                            'rgba(153, 102, 255, 1)',
+                            'rgba(80, 102, 255, 1)',
+                            'rgba(255, 159, 64, 1)'
+                        ],
+                        borderWidth: 1
+                    }]
+                },
+                options: {
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero: true
+                            }
+                        }]
+                    }
+                }
+            });
+        }
+        function llenarGraficaPie(id, d){
+            var ctxP = document.getElementById(id).getContext('2d');
+            //var ctx = document.getElementById("myChart").getContext('2d');            
+            var myPieChart = new Chart(ctxP, {
+                type: 'pie',
+                data: {
+                    //labels: ["Activos", "Pendientes", "Inactivos"],
+                    labels: d.labels,
+                    datasets: [{
+                        //data: [300, 50, 100],
+                        data: d.data,
+                        backgroundColor: ["#46BFBD", "#FDB45C", "#F7464A", "#949FB1", "#4D5360"],
+                        hoverBackgroundColor: ["#5AD3D1", "#FFC870", "#FF5A5E", "#A8B3C5", "#616774"]
+                    }]
+                },
+                options: {
+                    responsive: true
+                }
+            });
+        }
     </script>
 </body>
 </html>
